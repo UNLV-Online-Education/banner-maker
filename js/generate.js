47608600e4f9621174b46c8a459daf2b4629e9a6
Benjamin Root <benjamin.root@unlv.edu>
2020-06-04 16:19:36 -0700

    re-set starting banner to style 0

--- js/generate.js
+++ js/generate.js
@@ -4,7 +4,7 @@ var mainBlankLoaded = false;
 var moduleBlankLoaded = false;
 var firstTimeLoading = true;
 
-var currentStyle = allBannerStyles[2];
+var currentStyle = allBannerStyles[0];
 
 function init() {
   var form = document.getElementById('my-form');
